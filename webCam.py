460d6c5bbd3f8bedc4c800a1e8b268913986c7d9
V1n1r <vini.vini.rodriguesa@gmail.com>
2025-01-20 22:41:17 -0300

    Continuacao  aula captura e gravacao de videos

--- webCam.py
+++ webCam.py
@@ -4,19 +4,29 @@ cap = cv2.VideoCapture(0)
 lar = 420
 alt = 360
 
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+saida = cv2.VideoWriter('video_test.avi', fourcc, 20.0, (lar,alt))
+
 while True:
 
     ret, video = cap.read()
 
-    video1 = cv2.resize(video, (lar,alt), fx=0, fy=0, interpolation=cv2.INTER_AREA)
+    #video1 = cv2.resize(video, (lar,alt), fx=0, fy=0, interpolation=cv2.INTER_AREA)
 
-    cv2.imshow('Video', video1)
+    hsv = cv2.cvtColor(video, cv2.COLOR_BGR2HSV)
+
+    saida.write(hsv)
+
+    cv2.imshow('Video', video)
 
     key =  cv2.waitKey(1) & 0xFF
     if key == ord('q'):
         break
 
-cv2.destroyAllWindows()
+
+saida.relese()
 cap.relese()
+cv2.destroyAllWindows()
 
 # 16/01 https://www.youtube.com/watch?v=stCuBcDYatA - 16:39
+
